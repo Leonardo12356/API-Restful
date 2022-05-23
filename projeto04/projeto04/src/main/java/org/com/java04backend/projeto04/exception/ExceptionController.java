d1740843ebc7af37fdf3261738d7f09a0cc280cf
Leonardo Aguiar <leonardoaguiar1235@gmail.com>
2022-05-23 15:46:46 -0300

    Projeto banco

--- projeto04/projeto04/src/main/java/org/com/java04backend/projeto04/exception/ExceptionController.java
+++ projeto04/projeto04/src/main/java/org/com/java04backend/projeto04/exception/ExceptionController.java
@@ -10,7 +10,7 @@ public class ExceptionController {
 	@ExceptionHandler(ContaException.class)
 	public ResponseEntity<String> trataTodoNotFound(ContaException exception) {
 		String msg = String.format("O saldo da conta de ID %d não é suficiente.", exception.getId());
-		return ResponseEntity.notFound().header("x-erro-msg", msg).header("x-erro-code", "Saldo Insuficiente")
+		return ResponseEntity.badRequest().header("x-erro-msg", msg).header("x-erro-code", "Saldo Insuficiente")
 				.header("x-erro-value", exception.getId().toString()).build();
 	}
 }
